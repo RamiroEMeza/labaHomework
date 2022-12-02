52e51848df6f74698cda11a7b3028a787e03ce6d
ramiroMeza <rmeza.laba@solvd.com>
2022-12-01 21:32:21 -0300

    University Creator working

--- university/university/src/Main.java
+++ university/university/src/Main.java
@@ -1,9 +1,6 @@
-import university.administrativesections.College;
-import university.administrativesections.Speciality;
-import university.administrativesections.Subject;
 import university.administrativesections.University;
+import university.helpers.UniversityCreator;
 import university.members.Teacher;
-import university.quizes.Quiz;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -19,8 +16,14 @@ public class Main {
         int userRequest;
         BufferedReader readRequest = new BufferedReader(new InputStreamReader(System.in));
         do{
+            UniversityCreator universityCreator = new UniversityCreator();
             ArrayList<String> response;
             final int quantityOfTeachers = 5;
+            ArrayList<Teacher> teachers = new ArrayList<>();
+            for (int i = 1; i < (quantityOfTeachers+1); i++) {
+                teachers.add(new Teacher("Teacher "+i, i));
+            }
+
             final int quantityOfSubjects = 60;
             String[] colleges = {"Fine Arts", "Engineering and Technology", "Health Sciences"};
             String[] specialities = {"Anthropology", "Applied Plant Biology", "Astrophysics",
@@ -28,81 +31,22 @@ public class Main {
                                                      "Communication Sciences and Disorders"};
 
             //Create a university
-            University ohioUniversity = new University("Ohio U", 200);
-
-            //Create a university to compare
-            University ohioUniversity2 = new University("Ohio U", 200);
-
-            //add colleges to the university
-            for (int i = 0; i < colleges.length; i++) {
-                ohioUniversity.addCollege(new College(("College of "+colleges[i]),
-                                            Main.getRandomInt(100, 2000), (i+1)));
-                ohioUniversity2.addCollege(new College(("College of "+colleges[i]),
-                        Main.getRandomInt(100, 2000), (i+1)));
-            }
-
-            //DELETES EXAMPLE
-            //ohioUniversity.deleteCollege(2); //THIS WORKS
-
-            //add specialities to the colleges
-            for (int i = 0; i < specialities.length; i++) {
-                ohioUniversity.addSpeciality(Main.getRandomInt(1, (ohioUniversity.getColleges().size()-1)),
-                                            new Speciality(specialities[i], (i+1),
-                                                    Main.getRandomInt(100, 2000)));
-
-                ohioUniversity2.addSpeciality(Main.getRandomInt(1, (ohioUniversity.getColleges().size()-1)),
-                        new Speciality(specialities[i], (i+1),
-                                Main.getRandomInt(100, 2000)));
-            }
-
-            //add teachers to the university
-            for (int i = 1; i < (quantityOfTeachers+1); i++) {
-                ohioUniversity.addTeacher(new Teacher("Teacher "+i, i));
-                ohioUniversity2.addTeacher(new Teacher("Teacher "+i, i));
-            }
-
-            //add subjects to the specialities
-            for (int i = 1; i < (quantityOfSubjects+1); i++) {
-                ohioUniversity.addSubjectToSpeciality(Main.getRandomInt(1, ohioUniversity.getSpecialities().size()),
-                                                                new Subject(("Subject-"+i), 40,
-                                                                ohioUniversity.getTeacher(Main.getRandomInt(1,ohioUniversity.getTeachersQuantity())),
-                                                                new Quiz(4, 6, 0.7),
-                                                                Main.getRandomInt(50, 210)));
-                ohioUniversity2.addSubjectToSpeciality(Main.getRandomInt(1, ohioUniversity.getSpecialities().size()),
-                        new Subject(("Subject-"+i), 40,
-                                ohioUniversity.getTeacher(Main.getRandomInt(1,ohioUniversity.getTeachersQuantity())),
-                                new Quiz(4, 6, 0.7),
-                                Main.getRandomInt(50, 210)));
-            }
-
-            //DELETES EXAMPLES
-            //ohioUniversity.deleteSpeciality(2); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 1); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 2); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 3); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 4); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 5); //This WORKS
-//            ohioUniversity.deleteSubject("Subject-1", 6); //This WORKS
+            University ohioU = universityCreator.create("Ohio U", 200, colleges, specialities,
+                    teachers, quantityOfSubjects);
 
             System.out.println("\nOhio University is equals to Ohio University? " +
-                    ohioUniversity.equals(ohioUniversity) + "\n");
-
-            System.out.println("Ohio University is equals to other U? " +
-                    ohioUniversity.equals(new University("Chicago U", 400))+ "\n");
-
-            System.out.println("Ohio University is equals to Ohio University2? " +
-                    ohioUniversity.equals(ohioUniversity2)+ "\n");
+                    ohioU.equals(ohioU) + "\n");
 
             //Print colleges
-            System.out.println(ohioUniversity.getName()+" have these colleges:");
-            response = ohioUniversity.getColleges();
+            System.out.println(ohioU.getName()+" have these colleges:");
+            response = ohioU.getColleges();
             for (String word: response) {
                 System.out.println("-" + word);
             }
 
             //Print specialities
-            System.out.println("\n"+ohioUniversity.getName()+" have these specialities:");
-            response = ohioUniversity.getSpecialities();
+            System.out.println("\n"+ohioU.getName()+" have these specialities:");
+            response = ohioU.getSpecialities();
             for (String word: response) {
                 System.out.println("-" + word + "\n");
             }
@@ -112,28 +56,28 @@ public class Main {
                     System.out.println("\nIf you want to know the cost of any of ours specialities, please enter its " +
                                             "(id) number. " +
                                             "Any other character to exit");
-                    userRequest = Integer.valueOf(readRequest.readLine());
+                    userRequest = Integer.parseInt(readRequest.readLine());
                 }catch (Exception e) {
                     userRequest = 0;
                 }
 
-                if (userRequest > 0 && userRequest <= ohioUniversity.getLastSubjectId()) {
+                if (userRequest > 0 && userRequest <= ohioU.getLastSubjectId()) {
                     int specialityId = userRequest;
-                    String speciality = ohioUniversity.getSpecialityById(specialityId);
+                    String speciality = ohioU.getSpecialityById(specialityId);
                     System.out.println("\nHow much does it cost to study "+ speciality +" ("+ specialityId +") ?");
-                    System.out.println("--It will be: " + ohioUniversity.calculateCost(specialityId));
+                    System.out.println("--It will be: " + ohioU.calculateCost(specialityId));
 
                     try {//Ask user if he wants details about the speciality
                         System.out.println("\nIf you want to know the details of " + speciality + ", please enter [1]." +
                                 "\nAny other character to keep asking." + "\n[0] to exit");
-                        userRequest = Integer.valueOf(readRequest.readLine());
+                        userRequest = Integer.parseInt(readRequest.readLine());
                     }catch (Exception e) {
                         userRequest = -1;
                     }
 
                     if (userRequest == 1) {
                         System.out.println("\nIn " + speciality + " you will have:");
-                        response = ohioUniversity.getSpecialityInfo(specialityId);
+                        response = ohioU.getSpecialityInfo(specialityId);
                         for (String line: response) {
                             System.out.println(line);
                         }
---
+++ university/university/src/university/helpers/UniversityCreator.java
@@ -0,0 +1,49 @@
+package university.helpers;
+
+import university.administrativesections.College;
+import university.administrativesections.Speciality;
+import university.administrativesections.Subject;
+import university.administrativesections.University;
+import university.members.Teacher;
+import university.quizes.Quiz;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class UniversityCreator {
+    public static int getRandomInt(int min, int max){
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+    public University create(String name, int cost, String[] collegesNames, String[] specialitiesNames,
+                             ArrayList<Teacher> teachers, int subjectsQuantity){
+        //Create a university
+        University university = new University(name, cost);
+
+        //add colleges to the university
+        for (int i = 0; i < collegesNames.length; i++) {
+            university.addCollege(new College(("College of "+collegesNames[i]),
+                    UniversityCreator.getRandomInt(100, 2000), (i+1)));
+        }
+
+        //add specialities to the colleges
+        for (int i = 0; i < specialitiesNames.length; i++) {
+            university.addSpeciality(UniversityCreator.getRandomInt(1, (university.getColleges().size()-1)),
+                    new Speciality(specialitiesNames[i], (i+1),
+                            UniversityCreator.getRandomInt(100, 2000)));
+
+        }
+
+
+        //add subjects to the specialities
+        for (int i = 1; i < (subjectsQuantity+1); i++) {
+            university.addSubjectToSpeciality(UniversityCreator.getRandomInt(1, university.getSpecialities().size()),
+                    new Subject(("Subject-"+i), 40,
+                            teachers.get(UniversityCreator.getRandomInt(0,(teachers.size()-1))),
+                            new Quiz(4, 6, 0.7),
+                            UniversityCreator.getRandomInt(50, 210)));
+        }
+
+        return  university;
+    }
+
+}
